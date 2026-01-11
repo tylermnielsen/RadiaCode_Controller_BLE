3f672f185c166ab71c468d3653af0b939bb02ceb
Tyler Nielsen <tylernielsenn@gmail.com>
2026-01-10 20:07:44 -0700

    working .ini config

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # RadiaCode Controller BLE
+
+Requires Bluetooth Stack to be enabled with 
+```
+build_flags = -DPIO_FRAMEWORK_ARDUINO_ENABLE_BLUETOOTH
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,50 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <BluetoothHCI.h>
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+/**
+ * 1. Install the appropriate callbacks using the BluetoothHIDMaster::onXXX methods 
+ * 2. Start the Bluetooth processing with BluetoothHIDMaster::begin() or BluetoothHIDMaster::beginBLE() 
+ * 3. Connect to the first device found with BluetoothHIDMaster::connectXXX() and start receiving callbacks. 
+ * 4. Callbacks will come at interrupt time and set global state variables, which the main loop() will process
+ * 
+ */
+
+BluetoothHCI hci; 
+
+void setup(){
+  while(!Serial); 
+
+  Serial.begin(115200);
+
+  Serial.println("Starting Bluetooth HID Master"); 
+
+  l2cap_init();
+  gatt_client_init();
+  sm_init();
+  sm_set_io_capabilities(IO_CAPABILITY_NO_INPUT_NO_OUTPUT);
+  gap_set_default_link_policy_settings(LM_LINK_POLICY_ENABLE_SNIFF_MODE | LM_LINK_POLICY_ENABLE_ROLE_SWITCH);
+  hci_set_master_slave_policy(HCI_ROLE_MASTER);
+  hci_set_inquiry_mode(INQUIRY_MODE_RSSI_AND_EIR);
+
+  hci.setBLEName("Pico BLE Scanner");
+  hci.install();
+  hci.begin();
+
+
+  Serial.println("Setup Done."); 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.printf("BEGIN BLE SCAN @%lu ...", millis());
+  auto l = hci.scanBLE(BluetoothHCI::any_cod);
+  Serial.printf("END BLE SCAN @%lu\n\n", millis());
+  Serial.printf("%-8s | %-17s | %-4s | %s\n", "Class", "Address", "RSSI", "Name");
+  Serial.printf("%-8s | %-17s | %-4s | %s\n", "--------", "-----------------", "----", "----------------");
+  for (auto e : l) {
+    Serial.printf("%08lx | %17s | %4d | %s\n", e.deviceClass(), e.addressString(), e.rssi(), e.name());
+  }
+  Serial.printf("\n\n\n");
 }
