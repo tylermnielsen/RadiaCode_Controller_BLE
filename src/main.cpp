0e91c66a85f2961dfbec94b168577012d2b47325
Tyler Nielsen <tylernielsenn@gmail.com>
2026-01-10 22:59:49 -0700

    btstacklib scanner works

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,54 @@
 #include <Arduino.h>
 
-#include <BluetoothHCI.h>
+#include <BTstackLib.h>
+#include <SPI.h>
 
 /**
- * 1. Install the appropriate callbacks using the BluetoothHIDMaster::onXXX methods 
- * 2. Start the Bluetooth processing with BluetoothHIDMaster::begin() or BluetoothHIDMaster::beginBLE() 
- * 3. Connect to the first device found with BluetoothHIDMaster::connectXXX() and start receiving callbacks. 
- * 4. Callbacks will come at interrupt time and set global state variables, which the main loop() will process
- * 
+ * 1. Install the appropriate callbacks using the BluetoothHIDMaster::onXXX
+ * methods
+ * 2. Start the Bluetooth processing with BluetoothHIDMaster::begin() or
+ * BluetoothHIDMaster::beginBLE()
+ * 3. Connect to the first device found with BluetoothHIDMaster::connectXXX()
+ * and start receiving callbacks.
+ * 4. Callbacks will come at interrupt time and set global state variables,
+ * which the main loop() will process
+ *
  */
 
-BluetoothHCI hci; 
+int counter = 0;
+BD_ADDR device = BD_ADDR("52:43:06:60:17:dd");
 
-void setup(){
-  while(!Serial); 
+void advertisementCallback(BLEAdvertisement *bleAd);
+
+void setup() {
+  while (!Serial)
+    ;
 
   Serial.begin(115200);
 
-  Serial.println("Starting Bluetooth HID Master"); 
+  Serial.println("Starting Bluetooth HID Master");
 
-  l2cap_init();
-  gatt_client_init();
-  sm_init();
-  sm_set_io_capabilities(IO_CAPABILITY_NO_INPUT_NO_OUTPUT);
-  gap_set_default_link_policy_settings(LM_LINK_POLICY_ENABLE_SNIFF_MODE | LM_LINK_POLICY_ENABLE_ROLE_SWITCH);
-  hci_set_master_slave_policy(HCI_ROLE_MASTER);
-  hci_set_inquiry_mode(INQUIRY_MODE_RSSI_AND_EIR);
+  BTstack.setBLEAdvertisementCallback(advertisementCallback);
+  BTstack.setup();
+  BTstack.bleStartScanning();
 
-  hci.setBLEName("Pico BLE Scanner");
-  hci.install();
-  hci.begin();
-
-
-  Serial.println("Setup Done."); 
+  Serial.println("Setup Done.");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  BTstack.loop(); 
+}
 
-  Serial.printf("BEGIN BLE SCAN @%lu ...", millis());
-  auto l = hci.scanBLE(BluetoothHCI::any_cod);
-  Serial.printf("END BLE SCAN @%lu\n\n", millis());
-  Serial.printf("%-8s | %-17s | %-4s | %s\n", "Class", "Address", "RSSI", "Name");
-  Serial.printf("%-8s | %-17s | %-4s | %s\n", "--------", "-----------------", "----", "----------------");
-  for (auto e : l) {
-    Serial.printf("%08lx | %17s | %4d | %s\n", e.deviceClass(), e.addressString(), e.rssi(), e.name());
+void advertisementCallback(BLEAdvertisement *bleAd) {
+  if (!(bleAd->isIBeacon())) {
+    // Serial.print("Device discovered: ");
+    // Serial.println(bleAd->getBdAddr()->getAddressString());
+
+    if (memcmp(bleAd->getBdAddr()->getAddress(), device.getAddress(),
+               sizeof(device)) == 0) {
+      counter++;
+      Serial.printf("Found device %s, has been found %d times.\n",
+                    device.getAddressString(), counter);
+    }
   }
-  Serial.printf("\n\n\n");
 }
