02155391c2fd1ee47d3f005b3d9983d0d0a31f85
Tyler Nielsen <tylernielsenn@gmail.com>
2026-01-29 13:09:19 -0700

    working with new arduino-pico BLE, can connect, but no notificaitons

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <Arduino.h>
-#include <RadiaCodeBLELib.h>
+// #include <RadiaCodeBLELib.h>
+
+#include <BLE.h>
+#include "btstack.h"
 
 /**
  * 1. Install the appropriate callbacks using the BluetoothHIDMaster::onXXX
@@ -13,21 +16,82 @@
  *
  */
 
-String target_mac = "52:43:06:60:17:dd";
-RadiaCodeBLEController rc(target_mac);
+#define RADIACODE_SERVICE_UUID "e63215e5-7003-49d8-96b0-b024798fb901"
+#define RADIACODE_WRITE_FD_UUID "e63215e6-7003-49d8-96b0-b024798fb901"
+#define RADIACODE_NOTIFY_FD_UUID "e63215e7-7003-49d8-96b0-b024798fb901"
 
-void advertisementCallback(BLEAdvertisement* bleAd);
+
+String target_mac = "52:43:06:60:17:DD";
+// RadiaCodeBLEController rc(target_mac);
+// BLEAddress rc_addr(target_mac); 
+
+
+BLEUUID rc_service_uuid(RADIACODE_SERVICE_UUID);
+BLEUUID rc_write_uuid(RADIACODE_WRITE_FD_UUID);
+BLEUUID rc_notify_uuid(RADIACODE_NOTIFY_FD_UUID);
+
+BLERemoteCharacteristic* rc_write_char; 
+BLERemoteCharacteristic*rc_notify_char; 
+
+
+void notify(BLERemoteCharacteristic * c, const uint8_t * data, uint32_t len){
+  (void) c; 
+
+  Serial.println("Notify:");
+  Serial.write(data, len); 
+  Serial.println(); 
+}
 
 void setup() {
-  while (!Serial);
-
   Serial.begin(115200);
 
-  Serial.println("Starting Bluetooth HID Master");
+  while (!Serial);
 
-  rc.begin();
+  Serial.println("Starting BLE Client");
+
+  BLE.begin(); 
+
+  Serial.println("Scanning..."); 
+
+  BLEScanReport* res = BLE.scan();
+
+  for(BLEAdvertising item : *res){
+    if(item.getAddress().toString() == target_mac){
+      Serial.println("Found device, connecting...");
+      if(BLE.client()->connect(item) == false){
+        Serial.println("Connected."); 
+      } 
+      break;
+    }
+  }
+
+  Serial.println("Done."); 
+
+  Serial.println("Exploring service..."); 
+  BLERemoteService* service = BLE.client()->service(rc_service_uuid); 
+
+  if(service){
+
+    rc_notify_char = service->characteristic(rc_notify_uuid);
+    if(rc_notify_char){
+      Serial.println("Found notify char"); 
+      rc_notify_char->onNotify(notify); 
+      rc_notify_char->enableNotifications(); 
+
+    }
+    
+    rc_write_char = service->characteristic(rc_write_uuid); 
+    if(rc_write_char){
+      Serial.println("Found write char"); 
+    }
+
+  } else {
+    Serial.println("Error on service"); 
+  }
 
   Serial.println("Setup Done.");
 }
 
-void loop() { rc.loop(); }
+void loop() {
+
+}
